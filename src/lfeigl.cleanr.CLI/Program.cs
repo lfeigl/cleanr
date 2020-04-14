fdd5544f590d42a80e5189345d09fba2a5be2525
Luca Feigl <lfeigl.dev@gmail.com>
2020-04-14 21:27:33 +0200

    Add CLI for testing purposes

---
+++ src/lfeigl.cleanr.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lfeigl.cleanr.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
