c84bc8152e1f5d32063fa8232ad58f833b3d2984
Luca Feigl <lfeigl.dev@gmail.com>
2020-04-14 23:06:15 +0200

    Add Utils class with PrintBaseObject method

---
+++ src/lfeigl.cleanr.CLI/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using lfeigl.cleanr.Library;
+
+namespace lfeigl.cleanr.CLI
+{
+    public class Utils
+    {
+        public static void PrintBaseObject(Base baseObject)
+        {
+            List<string> print = new List<string>();
+
+            print.Add($"=== {baseObject.ID} ===");
+
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(baseObject))
+            {
+                string key = descriptor.Name;
+                object value = descriptor.GetValue(baseObject);
+
+                print.Add($"{key}: {value}");
+            }
+
+            print.Add("");
+
+            Console.WriteLine(string.Join("\n", print));
+        }
+    }
+}
