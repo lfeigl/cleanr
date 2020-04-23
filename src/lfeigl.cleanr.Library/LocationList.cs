de1489c53bd247f7952fef0daf8d9e7b08bfa469
Luca Feigl <lfeigl.dev@gmail.com>
2020-04-23 23:25:55 +0200

    Add LocationList class

---
+++ src/lfeigl.cleanr.Library/LocationList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace lfeigl.cleanr.Library
+{
+    public class LocationList : List<Location>
+    {
+        public bool AllChecked { get; set; }
+
+        public LocationList()
+        {
+            this.AllChecked = true;
+        }
+
+        public new void Add(Location location)
+        {
+            location.Checked = this.AllChecked;
+            base.Add(location);
+        }
+
+        public void ToggleAllChecked()
+        {
+            this.AllChecked = !this.AllChecked;
+
+            foreach (Location location in this)
+            {
+                location.Checked = this.AllChecked;
+            }
+        }
+    }
+}
