613912b1825f2e7da297819aefe8d25bed8b5c17
Luca Feigl <lfeigl.dev@gmail.com>
2020-05-07 21:10:12 +0200

    Use Metro UI

--- src/lfeigl.cleanr.GUI/MainWindow.xaml.cs
+++ src/lfeigl.cleanr.GUI/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Windows;
-using lfeigl.cleanr.Library;
+﻿using lfeigl.cleanr.Library;
 
 namespace lfeigl.cleanr.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
