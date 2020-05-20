d59e873fcfae6a6e02392ed0ae20da5a70af7bbf
Luca Feigl <lfeigl.dev@gmail.com>
2020-05-20 23:06:36 +0200

    Add static BaseDirectories class

---
+++ src/lfeigl.cleanr.Library/BaseDirectories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace lfeigl.cleanr.Library
+{
+    public static class BaseDirectories
+    {
+        public static string SystemDrive = Path.GetPathRoot(Environment.ExpandEnvironmentVariables("%SYSTEMROOT%"));
+        public static string UserProfile = Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+    }
+}
