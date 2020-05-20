4ae5fd10ab099c4517940bc8d3d7dfa159d22af4
Luca Feigl <lfeigl.dev@gmail.com>
2020-05-20 23:29:48 +0200

    Add missing backslash

--- src/lfeigl.cleanr.Library/BaseDirectories.cs
+++ src/lfeigl.cleanr.Library/BaseDirectories.cs
@@ -6,6 +6,6 @@ namespace lfeigl.cleanr.Library
     public static class BaseDirectories
     {
         public static string SystemDrive = Path.GetPathRoot(Environment.ExpandEnvironmentVariables("%SYSTEMROOT%"));
-        public static string UserProfile = Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+        public static string UserProfile = Environment.ExpandEnvironmentVariables("%USERPROFILE%") + @"\";
     }
 }
