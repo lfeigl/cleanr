8f5e0ece091920252bb3715993c5ecddba898ab5
Luca Feigl <lfeigl.dev@gmail.com>
2020-05-25 21:20:29 +0200

    Refactor static DefaultDirectories class

--- src/lfeigl.cleanr.Library/DefaultDirectories.cs
+++ src/lfeigl.cleanr.Library/DefaultDirectories.cs
@@ -2,14 +2,30 @@
 {
     public static class DefaultDirectories
     {
-        public static string ProgramData                = BaseDirectories.SystemDrive + @"ProgramData";
-        public static string ProgramFiles               = BaseDirectories.SystemDrive + @"Program Files";
-        public static string ProgramFilesCommonFiles    = BaseDirectories.SystemDrive + @"Program Files\Common Files";
-        public static string ProgramFilesX86            = BaseDirectories.SystemDrive + @"Program Files (x86)";
-        public static string ProgramFilesX86CommonFiles = BaseDirectories.SystemDrive + @"Program Files (x86)\Common Files";
-        public static string AppDataLocal               = BaseDirectories.UserProfile + @"AppData\Local";
-        public static string AppDataLocalLow            = BaseDirectories.UserProfile + @"AppData\LocalLow";
-        public static string AppDataRoaming             = BaseDirectories.UserProfile + @"AppData\Roaming";
-        public static string Documents                  = BaseDirectories.UserProfile + @"Documents";
+        public static string ProgramData { get; set; }
+        public static string ProgramFiles { get; set; }
+        public static string ProgramFilesCommonFiles { get; set; }
+        public static string ProgramFilesX86 { get; set; }
+        public static string ProgramFilesX86CommonFiles { get; set; }
+        public static string AppDataLocal { get; set; }
+        public static string AppDataLocalPrograms { get; set; }
+        public static string AppDataLocalProgramsCommon { get; set; }
+        public static string AppDataLocalLow { get; set; }
+        public static string AppDataRoaming { get; set; }
+        public static string Documents { get; set; }
+        static DefaultDirectories()
+        {
+            ProgramData                 = BaseDirectories.SystemDrive + @"ProgramData";
+            ProgramFiles                = BaseDirectories.SystemDrive + @"Program Files";
+            ProgramFilesCommonFiles     = BaseDirectories.SystemDrive + @"Program Files\Common Files";
+            ProgramFilesX86             = BaseDirectories.SystemDrive + @"Program Files (x86)";
+            ProgramFilesX86CommonFiles  = BaseDirectories.SystemDrive + @"Program Files (x86)\Common Files";
+            AppDataLocal                = BaseDirectories.UserProfile + @"AppData\Local";
+            AppDataLocalPrograms        = BaseDirectories.UserProfile + @"AppData\Local\Programs";
+            AppDataLocalProgramsCommon  = BaseDirectories.UserProfile + @"AppData\Local\Programs\Common";
+            AppDataLocalLow             = BaseDirectories.UserProfile + @"AppData\LocalLow";
+            AppDataRoaming              = BaseDirectories.UserProfile + @"AppData\Roaming";
+            Documents                   = BaseDirectories.UserProfile + @"Documents";
+        }
     }
 }
