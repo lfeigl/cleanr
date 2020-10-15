beaeaf6557cab38deca5274d5d4bfd3e648a1ccc
Luca Feigl <lfeigl.dev@gmail.com>
2020-10-15 21:44:49 +0200

    Add static DebugLog class

---
+++ src/lfeigl.cleanr.Library/DebugLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace lfeigl.cleanr.Library
+{
+    public static class DebugLog
+    {
+        private static string path = @"debug.log";
+
+        private static void Initialize()
+        {
+            string[] header = {
+                $"cleanr debug log [initialized on {DateTime.Now}]",
+                "",
+            };
+
+            File.WriteAllLines(path, header);
+        }
+
+        public static void Add(string log)
+        {
+            if (!File.Exists(path))
+            {
+                Initialize();
+            }
+
+            using (StreamWriter debugLog = new StreamWriter(path, true))
+            {
+                debugLog.WriteLine($"[{DateTime.Now}] {log}");
+            }
+        }
+    }
+}
